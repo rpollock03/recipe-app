a284a56ac81a576d647f24519d2e382e2d18230f
rpollock03 <49661708+rpollock03@users.noreply.github.com>
2020-02-15 18:10:11 +0000

    add first routes

--- app.js
+++ app.js
@@ -3,9 +3,19 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => res.send("Hellow world!"))
+// set templating engine to EJS
+app.set('view engine', 'ejs');
 
 
+//--
+// BASIC ROUTES
+//--
 
+app.get("/", (req, res) => res.render("landing"));
+
+app.get("/recipes", (req, res) => res.render("recipes"))
+
+
+// Port
 app.listen(port, () => console.log(`Server starting on port ${port}!`));
 
